60be6048858949629db2e8098de323efac80ae9b
BIJYO <132330398+BIJYO@users.noreply.github.com>
2023-05-08 11:57:55 +0530

    Create bijyo2.py

---
+++ bijyo2.py
@@ -0,0 +1,10 @@
+ID=9
+NAME="BIJITH"
+DEPARTMENT="CT"
+AGE=21
+COLLEGE="GPTC CHELAKKAREA"
+
+
+
+
+print("\t\t\t\t\tSTUDENT DETAILS\n\n\n","\t\t\t\t\tID IS :",ID,"\n","\t\t\t\t\tNAME IS : ",NAME,"\n","\t\t\t\t\tDEPARTMENT IS : ",DEPARTMENT,"\n","\t\t\t\t\tAGE IS : ",AGE,"\n","\t\t\t\t\tCOLLEGE IS : ",COLLEGE)
