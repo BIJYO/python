a1657c3a1de9e8007a74f6252605a02f527bedcf
BIJYO <132330398+BIJYO@users.noreply.github.com>
2023-05-08 15:37:51 +0530

    largestnum

---
+++ largestnum.py
@@ -0,0 +1,15 @@
+x=int(input("Enter the 1st Number : "))
+y= int(input("Enter the 2nd Number : "))
+z=int(input("enter the 3rd num : "))
+if x>y:
+    
+    if x>z:
+        print(x," is grater")
+   
+    else :
+        print(z,"is grater")
+elif y>z:
+    print(y,"is greater")
+   
+else :
+    print(z,"is Number greater ")
