89bc1ed7a60c68a25af0a3e636611b7775642769
BIJYO <132330398+BIJYO@users.noreply.github.com>
2023-05-09 12:03:15 +0530

    fibin.py

---
+++ fibin.py
@@ -0,0 +1,13 @@
+First = 0
+Second = 1
+Sum = 0
+i = 0
+
+while(i <  10):
+    print(First)
+    Sum = Sum + First
+    third = First + Second
+    First = Second
+    Second = third
+    i = i + 1
+
